25009dc605ff524895d87a50243cd81bad835bd8
noufanelachola <noufansadiqelachola@gmail.com>
2025-01-18 16:41:48 +0530

    arc added

--- src/components/Landing/components/Hero.js
+++ src/components/Landing/components/Hero.js
@@ -12,19 +12,20 @@ const Hero = () => {
                     <img className='logo' src={NavImage} alt='Nav Emoji' />
                     <p>For Freelancers, by Freelancers!</p>
                 </div>
-                <img src={NavLogo} alt="Nav Logo" className='navLogo' />
+                <img src={NavLogo} alt="Nav Logo" className='navLogo' style={{cursor:"pointer"}} />
                 <button className='btn btnPrimary clrWhite wgtSemiBold'>Get Started for Free</button>
             </nav>
             <div className='heroBody'>
                 <div>
                     <h1 className='textTitle wgtReg' >Create, Sign, and Share<br/>Documents Seamlessly</h1>
-                    <p className='textSubPara clrGray' style={{lineHeight:0,marginBottom:"1rem"}} >Smarter paperwork for freelancers</p>
-                    <button className='btn btnPrimary clrWhite textSubTitle'>Get Started for Free</button>
+                    <p className='textSubPara clrGray' style={{lineHeight:0,marginBottom:"1rem"}} >Smarter paperwork for freelancers!</p>
+                    <button className='btn btnPrimary clrWhite textSubTitle' style={{marginTop:"auto"}} >Get Started for Free</button>
                     <p className='textSubPara clrLightGray' style={{cursor:"pointer"}} ><u>Watch a Demo</u></p>
                 </div>
                 <img src={DashBoard} alt='Dashboard' />
             </div>
         </div>
+        <div className='arcDiv'></div>
     </section>
   )
 }
