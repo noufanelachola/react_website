6945b3b679594203c328209041fb1533f752179b
noufanelachola <noufansadiqelachola@gmail.com>
2025-01-18 15:46:16 +0530

    Hero page created

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Landing.css";
+import Hero from './components/Hero';
+
+const Landing = () => {
+  return (
+    <main>
+      <Hero/>
+    </main>
+  )
+}
+
+export default Landing
---
+++ src/components/Landing/components/Hero.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import NavImage from "../../../assets/nav_emoji.png";
+import NavLogo from "../../../assets/nav_logo.png";
+import DashBoard from "../../../assets/dashboard.png";
+
+const Hero = () => {
+  return (
+    <section className='sectionHero'>
+        <div className='content'>
+            <nav>
+                <div className='tagDiv'>
+                    <img className='logo' src={NavImage} alt='Nav Emoji' />
+                    <p>For Freelancers, by Freelancers!</p>
+                </div>
+                <img src={NavLogo} alt="Nav Logo" className='navLogo' />
+                <button className='btn btnPrimary clrWhite wgtSemiBold'>Get Started for Free</button>
+            </nav>
+            <div className='heroBody'>
+                <div>
+                    <h1 className='textTitle wgtReg' >Create, Sign, and Share<br/>Documents Seamlessly</h1>
+                    <p className='textSubPara clrGray' style={{lineHeight:0,marginBottom:"1rem"}} >Smarter paperwork for freelancers</p>
+                    <button className='btn btnPrimary clrWhite textSubTitle'>Get Started for Free</button>
+                    <p className='textSubPara clrLightGray' style={{cursor:"pointer"}} ><u>Watch a Demo</u></p>
+                </div>
+                <img src={DashBoard} alt='Dashboard' />
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
